43c2678e21baf087255200084ddfefef5be2adb3
Aniket Kale <aniketkale2003d@gmail.com>
2024-05-09 15:18:26 +0530

    Initial commit

---
+++ lib/home.dart
@@ -0,0 +1,88 @@
+import 'package:bwstory/profile.dart';
+import 'package:bwstory/search.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'explore.dart';
+import 'favourite.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final PageController pageController = PageController(initialPage: 0);
+  late int _selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true,
+      body: PageView(
+        controller: pageController,
+        children: const <Widget>[
+          Center(
+            child: Explore(),
+          ),
+          Center(
+            child: Search(),
+          ),
+          Center(
+            child: Favourite(),
+          ),
+          Center(
+            child: Profile(),
+          ),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.black,
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        notchMargin: 5.0,
+        clipBehavior: Clip.antiAlias,
+        child: SizedBox(
+          height: kBottomNavigationBarHeight,
+          child: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: Colors.black,
+            currentIndex: _selectedIndex,
+            selectedItemColor: Colors.white,
+            unselectedItemColor: Colors.grey,
+            onTap: (index) {
+              setState(() {
+                _selectedIndex = index;
+                pageController.jumpToPage(index);
+              });
+            },
+            items: const [
+
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined),
+                label: 'Explore',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                label: 'Search',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.favorite_border_outlined),
+                label: 'Favourites',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle_outlined),
+                label: 'Profile',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'authentication/phone.dart';
+import 'authentication/verify.dart';
+import 'home.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      name: "bwstory",
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyDOcl0yfMuE30l_kFCCLZKG5mtaWDoWMBM",
+          authDomain: "bwstory.firebaseapp.com",
+          projectId: "bwstory",
+          storageBucket: "bwstory.appspot.com",
+          messagingSenderId: "100332376572",
+          appId: "1:100332376572:web:6ce633152a17d91ee02279",
+          measurementId: "G-T1V8CV3XMY"));
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late StreamSubscription<User?> user;
+
+  @override
+  void initState() {
+    super.initState();
+    user = FirebaseAuth.instance.authStateChanges().listen((user) {
+      if (user == null) {
+        print('User is currently signed out!');
+      } else {
+        print('User is signed in!');
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    user.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      /// check if user is signed (Open Chat page ) if user is not signed in (open welcome page)
+      initialRoute:
+          FirebaseAuth.instance.currentUser == null ? 'phone' : 'home',
+
+      ///key value pair
+      routes: {
+        'phone': (context) => const MyPhone(),
+        'verify': (context) => const MyVerify(),
+        'home': (context) => const Home()
+      },
+      home: const MyPhone(),
+    );
+  }
+}
